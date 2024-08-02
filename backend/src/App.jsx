5e5c0c93e139e2476407cc27d5fc32c5b3bc62f5
lsb4486 <lsb4486@drexel.edu>
2024-08-02 19:40:03 -0400

    Started Authentication - Created placeholder Page

---
+++ backend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className=" text-white h-[100vh] flex justify-center items-center bg-cover "
+      style={{ background: "url('../src/assets/bg.jpg" }}
+    >
+      <Login />
+    </div>
+  );
+}
