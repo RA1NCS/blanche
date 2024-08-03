9520b8d2e3d6c96c24ad1b6f47dee41e1e14f09d
sb4486 <sb4486@drexel.edu>
2024-08-03 15:10:55 -0400

    Migrated to frontend, Refactored for Typescript compatibility

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,51 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Login = () => (
+	<div>
+		<h1>Login</h1>
+		<form action="">
+			<div>
+				<input type="email" />
+				<label htmlFor="">Your Email</label>
+			</div>
+			<div>
+				<input type="password" />
+				<label htmlFor="">Your Password</label>
+			</div>
+			<div>
+				<div>
+					<input
+						type="checkbox"
+						name=""
+						id="rememberMe"
+					/>
+					<label htmlFor="rememberMe">
+						Remember Me
+					</label>
+				</div>
+				<span>Forgot Password?</span>
+			</div>
+			<button type="submit">Login</button>
+			<div>
+				<span>
+					New Here?{' '}
+					<Link href="/register">
+						Create an Account
+					</Link>
+				</span>
+			</div>
+		</form>
+	</div>
+);
+
 export default function Home() {
-	return <h1>Authentication</h1>;
+	return (
+		<div
+			className="text-white h-[100vh] flex justify-center items-center bg-cover"
+			style={{ background: "url('/bg.jpg')" }}
+		>
+			<Login />
+		</div>
+	);
 }
---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Register = () => (
+	<div>
+		<h1>Register</h1>
+		<form action="">
+			<div>
+				<input type="text" />
+				<label htmlFor="">Your Name</label>
+			</div>
+			<div>
+				<input type="email" />
+				<label htmlFor="">Your Email</label>
+			</div>
+			<div>
+				<input type="password" />
+				<label htmlFor="">Your Password</label>
+			</div>
+			<button type="submit">Register</button>
+			<div>
+				<span>
+					Already have an account?{' '}
+					<Link href="/">Login here</Link>
+				</span>
+			</div>
+		</form>
+	</div>
+);
+
+export default Register;
