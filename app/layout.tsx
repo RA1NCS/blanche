RA1NCS/blanche

commit f11d18f6817a3060f80ebee6918906a1990213cb

Updated navbar rendering on all post-authentication pages

app/layout.tsx
@@ -1,18 +1,14 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import { headers } from 'next/headers';
 import { ClerkProvider } from '@clerk/nextjs';
 import Navbar from '@/components/Navbar/Navbar';
 
-export const metadata: Metadata = {
-	title: 'Whiteboard Learn',
-	description: 'A better way to learn.',
-};
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	const pathname = headers().get('X-Pathname') || '';
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+	const noNavbarRoutes = ['/', '/sign-up', '/sign-in'];
+
+	const hideNavbar = noNavbarRoutes.includes(pathname);
+
 	return (
 		<html lang="en">
 			<head>
@@ -24,6 +20,7 @@ export default function RootLayout({
 			<body>
 				<ClerkProvider>
 					<div className="flex h-screen">
+						{!hideNavbar && <Navbar />}
 						<main className="flex-1 p-10">
 							{children}
 						</main>

middleware.ts
@@ -10,6 +10,7 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware((auth, req) => {
 	const { userId } = auth();
+	const url = req.nextUrl;
 
 	if (isProtectedRoute(req)) {
 		if (!userId) {
@@ -17,7 +18,10 @@ export default clerkMiddleware((auth, req) => {
 		}
 	}
 
-	return NextResponse.next();
+	const response = NextResponse.next();
+	response.headers.set('X-Pathname', url.pathname);
+
+	return response;
 });
 
 export const config = {

commit 680fcad75ad818f9f046870313a9285219f4aa4b

[BUG FIX] Fixed website not rendering any CSS / JS
(Caused by commit f11d18f6817a3060f80ebee6918906a1990213cb)

app/layout.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from 'next';
+import './globals.css';
 import { headers } from 'next/headers';
 import { ClerkProvider } from '@clerk/nextjs';
 import Navbar from '@/components/Navbar/Navbar';
+
+export const metadata: Metadata = {
+	title: 'Whiteboard Learn',
+	description: 'A better way to learn.',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	const pathname = headers().get('X-Pathname') || '';
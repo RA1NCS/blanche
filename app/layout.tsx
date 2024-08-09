680fcad75ad818f9f046870313a9285219f4aa4b
Shreyan Gupta <64853271+RA1NCS@users.noreply.github.com>
2024-08-09 16:17:41 -0400

    [BUG FIX] Fixed website not rendering any CSS / JS
    (Caused by commit f11d18f6817a3060f80ebee6918906a1990213cb)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from 'next';
+import './globals.css';
 import { headers } from 'next/headers';
 import { ClerkProvider } from '@clerk/nextjs';
 import Navbar from '@/components/Navbar/Navbar';
+
+export const metadata: Metadata = {
+	title: 'Whiteboard Learn',
+	description: 'A better way to learn.',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	const pathname = headers().get('X-Pathname') || '';
