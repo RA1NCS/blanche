88b356a0492c9e1c5deab6787395acd7f7154c37
sb4486 <lsb4486@drexel.edu>
2024-08-08 21:06:35 -0400

    Authentication - Sign In with Clerk -Sign up with Clerk -Middleware to handle protected routes Note: This is just the working, not the frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'Whiteboard Learn',
-	description: 'A better way to learn.',
+  title: "Whiteboard Learn",
+  description: "A better way to learn.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<head>
-				<link
-					rel="icon"
-					href="/favicon.ico"
-				/>
-			</head>
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
+    </html>
+  );
 }
