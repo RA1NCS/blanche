05d691982d92670f9cb071d0ec106bcd323d2fd7
sb4486 <sb4486@drexel.edu>
2024-08-15 22:26:20 -0400

    CRITICAL FIX: Fixed issue where users were unable to sign into the application ISSUE: middleware wasn't redirecting if user was already authenticated / session was active.

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,32 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isProtectedRoute = createRouteMatcher(
-	process.env.NEXT_PUBLIC_POST_LOGIN_ROUTES?.split(',') || []
-);
+const isProtectedRoute = createRouteMatcher(process.env.NEXT_PUBLIC_POST_LOGIN_ROUTES?.split(',') || []);
 
 export default clerkMiddleware((auth, req) => {
 	const { userId } = auth();
 	const url = req.nextUrl;
-	let response;
+	const loginRoute = new URL('/', req.url);
+	const coursesRoute = new URL('/courses', req.url);
 
-	if (isProtectedRoute(req)) {
-		if (!userId) {
-			response = NextResponse.redirect(new URL('/', req.url));
-		} else {
-			response = NextResponse.next();
-		}
-	} else {
-		response = NextResponse.next();
+	// If user is authenticated and is on the login page, redirect to /courses
+	if (userId && url.pathname === '/') {
+		return NextResponse.redirect(coursesRoute);
 	}
 
-	response.headers.set('X-Pathname', url.pathname);
+	// If user is not authenticated and tries to access a protected route, redirect to login
+	if (isProtectedRoute(req) && !userId) {
+		return NextResponse.redirect(loginRoute);
+	}
 
+	// Proceed as normal
+	const response = NextResponse.next();
+	response.headers.set('X-Pathname', url.pathname);
 	return response;
 });
 
 export const config = {
 	matcher: [
-		// Ensure the matcher covers the correct routes
 		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
 		'/(api|trpc)(.*)',
 	],
